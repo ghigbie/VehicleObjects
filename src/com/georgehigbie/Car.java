d2ac81b1546a57e035dc5ee2050a0b2bb08b65f7
George Higbie <gehigbie@gmail.com>
2017-01-28 16:36:38 -0700

    add method to car object

--- src/com/georgehigbie/Car.java
+++ src/com/georgehigbie/Car.java
@@ -3,5 +3,17 @@ package com.georgehigbie;
 /**
  * Created by georgehigbie on 1/28/17.
  */
-public class Car {
+public class Car extends Vehicle {
+
+    public Car(String steering, int currentGear, int movingSpeedMax, int movingSpeedOptimum) {
+        super(steering, currentGear, movingSpeedMax, movingSpeedOptimum);
+    }
+
+    @Override
+    public void move(int movingSpeed) {
+        System.out.println("This car is moving at " + movingSpeed + "mph.");
+        super.move(movingSpeed);
+    }
+
+
 }
