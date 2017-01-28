5d9c28d41858be47a4b92afa74bb67f085bfe092
George Higbie <gehigbie@gmail.com>
2017-01-28 16:57:37 -0700

    adde change gears method to vechilce class

--- src/com/georgehigbie/Vehicle.java
+++ src/com/georgehigbie/Vehicle.java
@@ -38,4 +38,45 @@ public class Vehicle {
         System.out.println("This vehicle is moving fast!");
     }
 
+    public void changingGearsUp(int change){
+        int changeGears = getCurrentGear() + change;
+        System.out.println("You just up shifted from gear " + getCurrentGear() + " to " +changeGears + ".");
+    }
+
+    public void changingGearsDown(int change){
+        int changeGears = getCurrentGear() - change;
+        System.out.println("You just down shifted from gear " + getCurrentGear() + " to " +changeGears + ".");
+    }
+
+    public String getSteering() {
+        return steering;
+    }
+
+    public void setSteering(String steering) {
+        this.steering = steering;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
+
+    public int getMovingSpeedMax() {
+        return movingSpeedMax;
+    }
+
+    public void setMovingSpeedMax(int movingSpeedMax) {
+        this.movingSpeedMax = movingSpeedMax;
+    }
+
+    public int getMovingSpeedOptimum() {
+        return movingSpeedOptimum;
+    }
+
+    public void setMovingSpeedOptimum(int movingSpeedOptimum) {
+        this.movingSpeedOptimum = movingSpeedOptimum;
+    }
 }
