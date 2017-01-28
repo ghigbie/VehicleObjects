5d2886fa67272b7e44a2e4ad58310cf3014f97c5
George Higbie <gehigbie@gmail.com>
2017-01-28 16:59:00 -0700

    added test cases

--- src/com/georgehigbie/Main.java
+++ src/com/georgehigbie/Main.java
@@ -12,8 +12,10 @@ public class Main {
     public static void manufactureCars(){
         Car car1 = new Car("Power Steering", 1, 120, 55);
         car1.move(66);
+        car1.changingGearsDown(1);
 
         Honda honda1 = new Honda("Power Steering", 2, 130, 55);
         honda1.move(77);
+        honda1.changingGearsUp(2);
     }
 }
